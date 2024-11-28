f55a335bfe9d9897a8945e95d3e6c6f1b58441df
Abraxas-7 <arber.beshaj.02@gmail.com>
2024-11-28 14:50:12 +0100

    hello + parole

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
-console.log("hello world");
+// console.log("hello world");
 
-console.log("Hello boolean");
+// console.log("Hello boolean");
+
+const args = process.argv;
+
+console.log(args); // stampa tutti i componenti dell'array
+
+console.log(`Hello ${args[2]}`); // Stampa solo la prima parola passata come argomento
+
+console.log(`Hello ${args.splice(2).join(" ")}`); // Stampa tutte le parole passate come argomenti
